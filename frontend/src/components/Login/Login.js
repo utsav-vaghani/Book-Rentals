dd716e4c1004000cded009afd12bb9c1b5b3288b
dhairya1431 <dhairyapatel610@gmail.com>
2020-12-08 11:11:48 +0530

    Changed frontend files(added states)

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,22 +1,34 @@
 import React from "react";
+import { useState } from 'react'
 import avatar from "../../images/avatar.png";
 import "../../App.css";
 import { Link } from "react-router-dom";
-const Login = () => {
+
+function Login() {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const submit_handler = (e) => {
+    e.preventDefault();
+    console.log(username + password)
+  }
   return (
     <div className="register">
       <img src={avatar} className="avatar" alt="" />
       <h1>LogIn</h1>
       <form>
         <p>Email</p>
-        <input type="email" name="email" placeholder="Enter Email..." />
+        <input type="email" name="email" placeholder="Enter Email..." value={username} onChange={(e) => setUsername(e.target.value)} />
         <p>Password</p>
         <input
           type="password"
           name="password"
           placeholder="Enter Password..."
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <input type="submit" name="" value="Login" />
+        <input type="submit" onClick={submit_handler} />
         <Link to="/account/register">Don't Have An Account?</Link>
         {/* TODO */}
         <Link to="/">Forgot Your Password</Link>
@@ -26,3 +38,4 @@ const Login = () => {
 };
 
 export default Login;
+
--- frontend/src/components/Register/Register.js
+++ frontend/src/components/Register/Register.js
@@ -1,29 +1,42 @@
-import React from "react";
+import React , {useState} from "react";
 import { Link } from "react-router-dom";
 import "../../App.css";
 import avatar from "../../images/avatar.png";
 
-const Register = () => {
+function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("")
+  const [address, setAddress] = useState("")
+  const [password, setPassword] = useState("")
+  const [re_password, setRe_password] = useState("")
+
+ const submit_handler = e => {
+    e.preventDefault();
+    console.log(name + email + address + password);
+
+    //After logic of submit
+  }
   return (
     <div className="register">
       <img src={avatar} className="avatar" alt="" />
       <h1>Register Here</h1>
       <form>
         <p>Name</p>
-        <input type="text" name="name" placeholder="Enter Name..." />
+        <input type="text" name="name" placeholder="Enter Name..." value={name} onChange={e => setName(e.target.value)}/>
         <p>Email</p>
-        <input type="email" name="email" placeholder="Enter Email..." />
+        <input type="email" name="email" placeholder="Enter Email..." value={email} onChange={e => setEmail(e.target.value)}/>
         <p>Address</p>
-        <textarea name="address" placeholder="Enter Address.." />
+        <textarea name="address" placeholder="Enter Address.." value={address} onChange={e => setAddress(e.target.value)}/>
         <p>Password</p>
-        <input type="password" name="password" placeholder="Enter Password" />
+        <input type="password" name="password" placeholder="Enter Password" value={password} onChange={e => setPassword(e.target.value)}/>
         <p>Confirm Password</p>
         <input
           type="password"
           name="cfpassword"
           placeholder="Enter Confirm Password"
+          value={re_password} onChange={e => setRe_password(e.target.value)}
         />
-        <input type="submit" name="" value="SignUp" />
+        <input type="submit" name="" value="SignUp" onClick={submit_handler} />
         <Link href="/accoutn/signin">Already Have An Account</Link>
       </form>
     </div>
